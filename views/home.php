6c1a8ddb27cc50f6df214a1293e924a4471ef3e1
milos <milos.vidakovic.22@singimail.rs>
2024-11-24 11:51:44 +0100

    Update

--- views/home.php
+++ views/home.php
@@ -1,36 +1,9 @@
-<div class="card card-plain">
-    <div class="card-header pb-0 text-start">
-        <h4 class="font-weight-bolder">Compare</h4>
+<section>
+    <div class="text-center" data-bss-scroll-zoom="true" data-bss-scroll-zoom-speed="1" style="background-image: url(&quot;assets/img/Screenshot%202024-11-23%20202324.png&quot;);background-position: center;background-size: cover;">
+        <div class="text-center d-flex flex-column justify-content-center align-items-center py-5 px-5 hero-content mt-0" data-bss-scroll-zoom="true" data-bss-scroll-zoom-speed="1" style="background-image: url(&quot;assets/img/Screenshot%202024-11-23%20202324.png&quot;);background-position: center;background-size: cover;">
+            <h1 class="display-1" style="font-weight: bold;font-size: 46.64px;">compare computer components</h1>
+            <p class="fs-3" style="font-weight: bold;">Graphics cards, processors, storage and more to come</p>
+            <form class="d-sm-flex d-md-flex align-items-sm-center justify-content-md-center align-items-md-center"><input class="bg-dark-subtle border rounded-pill form-control ms-0" type="search" placeholder="Type here to compare" autocomplete="on" style="width: 340px;height: 48px;padding-left: 10px;font-size: 20px;"><input class="btn btn-primary" type="submit" value="Compare"></form>
+        </div>
     </div>
-    <div class="card-body">
-        <form role="form" method="post" action="/compare">
-            <div class="mb-3">
-                <input type="text" name="text" class="form-control form-control-lg" placeholder="Product 1" aria-label="Text">
-                <?php
-                if ($params != null && $params->errors != null) {
-                    foreach ($params->errors as $attribute => $error) {
-                        if ($attribute == 'email') {
-                            echo "<span class='text-danger'>$error[0]</span>";
-                        }
-                    }
-                }
-                ?>
-            </div>
-            <div class="mb-3">
-                <input type="text" name="text" class="form-control form-control-lg" placeholder="Product 2" aria-label="Text">
-                <?php
-                if ($params != null && $params->errors != null) {
-                    foreach ($params->errors as $attribute => $error) {
-                        if ($attribute == 'password') {
-                            echo "<span class='text-danger'>$error[0]</span>";
-                        }
-                    }
-                }
-                ?>
-            </div>
-            <div class="text-center">
-                <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Compare</button>
-            </div>
-        </form>
-    </div>
-</div>
+</section>
