6c15d8f312cecc3e44108dd7ed4ca4c4d104b926
Goran Suvacarev <goran.suvacarev.22@singimail.rs>
2024-11-26 13:20:40 +0100

    -

--- views/addCPU.php
+++ views/addCPU.php
@@ -1,52 +1,55 @@
 
 <section class="clean-block clean-form dark">
     <div class="container d-flex justify-content-center align-items-center mt-6">
-        <form role="form" method="post" class="text-start" style="margin-top: 120px;" action="/processAddCPU">
-            <div class="mb-3"><label class="form-label" for="username">Name</label><input class="form-control" type="text" name="name" id="name" data-bs-theme="light">
+        <form role="form" method="post" class="text-start" style="margin-top: 60px; margin-bottom: 60px" action="/processAddCPU">
 
-            </div>
-            <div class="mb-3"><label class="form-label" for="email">Price</label><input class="form-control item" type="text" name="price" id="price"  data-bs-theme="light">
-
-            </div>
-            <div class="mb-3"><label class="form-label" for="password">Image</label><input class="form-control" type="text" name="image" id="image" data-bs-theme="light">
-
+            <div class="mb-3"><label class="form-label" >Name</label><input required class="form-control" type="text" name="name" id="name" data-bs-theme="light">
             </div>
 
-            <div class="mb-3"><label class="form-label" for="password">socket</label><input class="form-control" type="text" name="socket" id="clock_speed" data-bs-theme="light">
-
-            </div>
-            <div class="mb-3"><label class="form-label" for="password">chipset</label><input class="form-control" type="text" name="chipset" id="turbo_speed" data-bs-theme="light">
-
-            </div>
-            <div class="mb-3"><label class="form-label" for="password">semiconductor_size</label><input class="form-control" type="text" name="semiconductor_size" id="pixel_rate" data-bs-theme="light">
-
+            <div class="mb-3"><label class="form-label" >Price</label><input required  class="form-control item" type="text" name="price" id="price"  data-bs-theme="light">
             </div>
 
-            <div class="mb-3"><label class="form-label" for="password">thermal_design_power</label><input class="form-control" type="text" name="thermal_design_power" id="texture_mapping_units" data-bs-theme="light">
-
+            <div class="mb-3"><label class="form-label" >Image</label><input required class="form-control" type="text" name="image" id="image" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">clock_speed</label><input class="form-control" type="text" name="clock_speed" id="render_output_units" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >socket</label><input required class="form-control" type="text" name="socket" id="clock_speed" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">turbo_speed</label><input class="form-control" type="text" name="turbo_speed" id="vram_size" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >chipset</label><input required class="form-control" type="text" name="chipset" id="turbo_speed" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">threads</label><input class="form-control" type="text" name="threads" id="memory_bus_width" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >semiconductor_size</label><input required class="form-control" type="text" name="semiconductor_size" id="pixel_rate" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">cores</label><input class="form-control" type="text" name="cores" id="memory_bus_width" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >thermal_design_power</label><input required class="form-control" type="text" name="thermal_design_power" id="texture_mapping_units" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">l1_cache</label><input class="form-control" type="text" name="l1_cache" id="memory_bus_width" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >clock_speed</label><input required class="form-control" type="text" name="clock_speed" id="render_output_units" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">l2_cache</label><input class="form-control" type="text" name="l2_cache" id="memory_bus_width" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >turbo_speed</label><input required class="form-control" type="text" name="turbo_speed" id="vram_size" data-bs-theme="light">
             </div>
-            <div class="mb-3"><label class="form-label" for="password">l3_cache</label><input class="form-control" type="text" name="l3_cache" id="memory_bus_width" data-bs-theme="light">
 
+            <div class="mb-3"><label class="form-label" >threads</label><input required class="form-control" type="text" name="threads" id="memory_bus_width" data-bs-theme="light">
             </div>
-            <div class="mb-3"></div><button class="btn btn-primary text-center align-items-xxl-center" type="submit">Add</button>
+
+            <div class="mb-3"><label class="form-label" >cores</label><input required class="form-control" type="text" name="cores" id="memory_bus_width" data-bs-theme="light">
+            </div>
+
+            <div class="mb-3"><label class="form-label" >l1_cache</label><input required class="form-control" type="text" name="l1_cache" id="memory_bus_width" data-bs-theme="light">
+            </div>
+
+            <div class="mb-3"><label class="form-label" >l2_cache</label><input required class="form-control" type="text" name="l2_cache" id="memory_bus_width" data-bs-theme="light">
+            </div>
+
+            <div class="mb-3"><label class="form-label" >l3_cache</label><input required class="form-control" type="text" name="l3_cache" id="memory_bus_width" data-bs-theme="light">
+            </div>
+
+            <div class="mb-3">
+            </div>
+
+            <button class="btn btn-primary text-center align-items-xxl-center" type="submit">Add</button>
+
         </form>
     </div>
 </section>
