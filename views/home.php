b641c3d80d58d38785865e34cb9cbd18d844fb35
Goran Suvacarev <goran.suvacarev.22@singimail.rs>
2024-11-27 10:24:18 +0100

    -

--- views/home.php
+++ views/home.php
@@ -25,106 +25,107 @@ use app\models\SuggestionModel;
         if(Application::$app->session->get('user')){
             if($_SESSION['user'][0]['role'] == 'Admin'){
                 echo "
-                    <div class='card'>
-    <div class='card-body'>
-        <div class='row'>
-            <div class='col-md-12'>
+        <div class='card'>
+            <div class='card-body'>
                 <div class='row'>
-                    <div class='col-md-6'>
-                        <label for='from'>From:</label>
-                        <input type='date' class='form-control date-helper-suggestions' placeholder='From' id='from'>
+                    <div class='col-md-12'>
+                        <div class='row'>
+                            <div class='col-md-6'>
+                                <label for='from'>From:</label>
+                                <input type='date' class='form-control date-helper-suggestions' placeholder='From' id='from'>
+                            </div>
+                            <div class='col-md-6'>
+                                <label for='from'>To:</label>
+                                <input type='date' class='form-control date-helper-suggestions' placeholder='To' id='to'>
+                            </div>
+                        </div>
+                        <div class='chart'>
+                            <div id='suggestions-per-product-canvas'>
+                                <canvas id='suggestions-per-product'
+                                        style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
+                                        class='chartjs-render-monitor'></canvas>
+                            </div>
+                        </div>
+                        <div class='row'>
+                            <div class='col-md-6'>
+                                <label for='from'>From:</label>
+                                <input type='date' class='form-control date-helper-views' placeholder='From' id='fromView'>
+                            </div>
+                            <div class='col-md-6'>
+                                <label for='from'>To:</label>
+                                <input type='date' class='form-control date-helper-views' placeholder='To' id='toView'>
+                            </div>
+                        </div>
+                        <div class='chart'>
+                            <div id='views-per-gpu-canvas'>
+                                <canvas id='views-per-gpu'
+                                        style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
+                                        class='chartjs-render-monitor'></canvas>
+                            </div>
+                        </div>
+                        <div class='chart'>    
+                            <div id='views-per-cpu-canvas'>
+                                <canvas id='views-per-cpu'
+                                        style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
+                                        class='chartjs-render-monitor'></canvas>
+                            </div>
+                        </div>
+                        <div class='chart'>
+                            <div id='views-per-ssd-canvas'>
+                                <canvas id='views-per-ssd'
+                                        style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
+                                        class='chartjs-render-monitor'></canvas>
+                            </div>
+                        </div>
                     </div>
                     <div class='col-md-6'>
-                        <label for='from'>To:</label>
-                        <input type='date' class='form-control date-helper-suggestions' placeholder='To' id='to'>
                     </div>
                 </div>
-                <div class='chart'>
-                    <div id='suggestions-per-product-canvas'>
-                        <canvas id='suggestions-per-product'
-                                style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
-                                class='chartjs-render-monitor'></canvas>
-                    </div>
-                </div>
-                <div class='row'>
-                    <div class='col-md-6'>
-                        <label for='from'>From:</label>
-                        <input type='date' class='form-control date-helper-views' placeholder='From' id='fromView'>
-                    </div>
-                    <div class='col-md-6'>
-                        <label for='from'>To:</label>
-                        <input type='date' class='form-control date-helper-views' placeholder='To' id='toView'>
-                    </div>
-                </div>
-                <div class='chart'>
-                    <div id='views-per-gpu-canvas'>
-                        <canvas id='views-per-gpu'
-                                style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
-                                class='chartjs-render-monitor'></canvas>
-                    </div>
-                </div>
-                <div class='chart'>    
-                    <div id='views-per-cpu-canvas'>
-                        <canvas id='views-per-cpu'
-                                style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
-                                class='chartjs-render-monitor'></canvas>
-                    </div>
-                </div>
-                <div class='chart'>
-                    <div id='views-per-ssd-canvas'>
-                        <canvas id='views-per-ssd'
-                                style='min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 634px;'
-                                class='chartjs-render-monitor'></canvas>
-                    </div>
-                </div>
-            </div>
-            <div class='col-md-6'>
             </div>
         </div>
-    </div>
-</div>
                 ";
             }
             else{
-                echo "<div class='container position-relative' style='margin-top: 5px;'>
-    <div class='row d-flex justify-content-center'>
-        <div class='col-md-8 col-lg-6 col-xl-5 col-xxl-4'>
-            <div class='card mb-5'>
-                <div class='card-body p-sm-5' style='width: 419px;'>
-                    <h2 class='text-center mb-4' style='font-size: 32px;'>Suggest a product</h2>
-                    <form method='post' action='/addSuggestion'><span style='font-size: 20px;margin-top: 0px;margin-bottom: 0px;padding-bottom: 0px;'>Product name</span>
-                        <input type='hidden' name='id_users' value='"; echo $_SESSION['user'][0]['id_user'] ?? 0; echo"'>
-                        <div class='mb-3'><input class='form-control' type='text' id='name' name='name' placeholder='Write the name here'></div><span style='font-size: 20px;'>Sources (Optional)</span>
-                        <div class='mb-3'><textarea class='form-control' id='source' name='source' rows='6' placeholder='Where can we find more inforamtion' style='max-height: 200px;min-height: 50px;height: 170;'></textarea></div>
-                        <div><button class='btn btn-primary d-block w-100' type='submit'>Send</button></div>
-                    </form>
+                echo "
+        <div class='container position-relative' style='margin-top: 5px;'>
+            <div class='row d-flex justify-content-center'>
+                <div class='col-md-8 col-lg-6 col-xl-5 col-xxl-4'>
+                    <div class='card mb-5'>
+                        <div class='card-body p-sm-5' style='width: 419px;'>
+                            <h2 class='text-center mb-4' style='font-size: 32px;'>Suggest a product</h2>
+                            <form method='post' action='/addSuggestion'><span style='font-size: 20px;margin-top: 0px;margin-bottom: 0px;padding-bottom: 0px;'>Product name</span>
+                                <input type='hidden' name='id_users' value='"; echo $_SESSION['user'][0]['id_user'] ?? 0; echo"'>
+                                <div class='mb-3'><input class='form-control' type='text' id='name' name='name' placeholder='Write the name here'></div><span style='font-size: 20px;'>Sources (Optional)</span>
+                                <div class='mb-3'><textarea class='form-control' id='source' name='source' rows='6' placeholder='Where can we find more inforamtion' style='max-height: 200px;min-height: 50px;height: 170;'></textarea></div>
+                                <div><button class='btn btn-primary d-block w-100' type='submit'>Send</button></div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>" ;
+        </div>" ;
             }
 
         }
         else{
             echo"
-     <div class='container position-relative' style='margin-top: 5px;'>
-    <div class='row d-flex justify-content-center'>
-        <div class='col-md-8 col-lg-6 col-xl-5 col-xxl-4'>
-            <div class='card mb-5'>
-                <div class='card-body p-sm-5' style='width: 419px;'>
-                    <h2 class='text-center mb-4' style='font-size: 32px;'>Suggest a product</h2>
-                    <form method='post' action='/addSuggestion'><span style='font-size: 20px;margin-top: 0px;margin-bottom: 0px;padding-bottom: 0px;'>Product name</span>
-                        <input type='hidden' name='id_users' value='"; echo $_SESSION['user'][0]['id_user'] ?? 0; echo "'>
-                        <div class='mb-3'><input class='form-control' type='text' id='name' name='name' placeholder='Write the name here'></div><span style='font-size: 20px;'>Sources (Optional)</span>
-                        <div class='mb-3'><textarea class='form-control' id='source' name='source' rows='6' placeholder='Where can we find more inforamtion' style='max-height: 200px;min-height: 50px;height: 170;'></textarea></div>
-                        <div><button class='btn btn-primary d-block w-100' type='submit'>Send</button></div>
-                    </form>
+        <div class='container position-relative' style='margin-top: 5px;'>
+            <div class='row d-flex justify-content-center'>
+                <div class='col-md-8 col-lg-6 col-xl-5 col-xxl-4'>
+                    <div class='card mb-5'>
+                        <div class='card-body p-sm-5' style='width: 419px;'>
+                            <h2 class='text-center mb-4' style='font-size: 32px;'>Suggest a product</h2>
+                            <form method='post' action='/addSuggestion'><span style='font-size: 20px;margin-top: 0px;margin-bottom: 0px;padding-bottom: 0px;'>Product name</span>
+                                <input type='hidden' name='id_users' value='"; echo $_SESSION['user'][0]['id_user'] ?? 0; echo "'>
+                                <div class='mb-3'><input class='form-control' type='text' id='name' name='name' placeholder='Write the name here'></div><span style='font-size: 20px;'>Sources (Optional)</span>
+                                <div class='mb-3'><textarea class='form-control' id='source' name='source' rows='6' placeholder='Where can we find more inforamtion' style='max-height: 200px;min-height: 50px;height: 170;'></textarea></div>
+                                <div><button class='btn btn-primary d-block w-100' type='submit'>Send</button></div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
             ";
         }
         ?>
